8001ced69be72c04cbc1c93a5e1bfb8b2678de65
jtm142269 <yamitnakra1990@gmail.com>
2014-09-24 20:40:44 +0530

    message

---
+++ 1.py
@@ -0,0 +1,220 @@
+#!/usr/bin/python
+
+with open('content.txt','r') as f:
+    Happy = 0
+    Sad = 0
+    Sarcastic = 0
+    Surprised = 0
+    Crook = 0
+    Neutral = 0
+    Angry = 0
+    for line in f.readlines():
+        if line[0] == "A":
+                for word in line.split():
+                   
+
+                    if word == ":)" or word == ":D":
+                            Happy = Happy + 1
+                    elif word == ":(" or word == ":'(" :
+                            Sad = Sad + 1
+                    elif word == ":P" or word == ";)" :
+                            Sarcastic = Sarcastic + 1
+                    elif word == ":-o" or word == "O_O" :
+                            Surprised = Surprised + 1
+                    elif word == "B-)" or word == ";)" :
+                            Crook = Crook + 1
+                    elif word == ":-/" or word == "=_=" :
+                            Neutral = Neutral + 1
+                    elif word == "x-(" or word == ">_<" :
+                            Angry = Angry + 1
+    print"Emoticons for A is"
+    print"Happy=",Happy
+    print"Sad=",Sad
+    print"Sarcastic=",Sarcastic
+    print"Surprised=",Surprised
+    print"Crook=",Crook
+    print"Neutral=",Neutral
+    print"Angry=",Angry
+with open('content.txt','r') as f:
+    Happy = 0
+    Sad = 0
+    Sarcastic = 0
+    Surprised = 0
+    Crook = 0
+    Neutral = 0
+    Angry = 0   
+           
+       
+           
+    for line in f.readlines():       
+
+        if line[0] == "B":
+                   for word in line.split():
+                    if word == ":)" or word == ":D":
+                            Happy = Happy + 1
+                    elif word == ":(" or word == ":'(" :
+                            Sad = Sad + 1
+                    elif word == ":P" or word == ";)" :
+                            Sarcastic = Sarcastic + 1
+                    elif word == ":-o" or word == "O_O" :
+                            Surprised = Surprised + 1
+                    elif word == "B-)" or word == ";)" :
+                            Crook = Crook + 1
+                    elif word == ":-/" or word == "=_=" :
+                            Neutral = Neutral + 1
+                    elif word == "x-(" or word == ">_<" :
+                            Angry = Angry + 1
+    print"Emoticons for B is"
+    print"Happy=",Happy
+    print"Sad=",Sad
+    print"Sarcastic=",Sarcastic
+    print"Surprised=",Surprised
+    print"Crook=",Crook
+    print"Neutral=",Neutral
+    print"Angry=",Angry
+
+with open('content.txt','r') as f:
+    Happy = 0
+    Sad = 0
+    Sarcastic = 0
+    Surprised = 0
+    Crook = 0
+    Neutral = 0
+    Angry = 0
+    for line in f.readlines():
+        if line[0] == "C":
+                   for word in line.split():
+                    if word == ":)" or word == ":D":
+                            Happy = Happy + 1
+                    elif word == ":(" or word == ":'(" :
+                            Sad = Sad + 1
+                    elif word == ":P" or word == ";)" :
+                            Sarcastic = Sarcastic + 1
+                    elif word == ":-o" or word == "O_O" :
+                            Surprised = Surprised + 1
+                    elif word == "B-)" or word == ";)" :
+                            Crook = Crook + 1
+                    elif word == ":-/" or word == "=_=" :
+                            Neutral = Neutral + 1
+                    elif word == "x-(" or word == ">_<" :
+                            Angry = Angry + 1
+    print"Emoticons for C is"
+    print"Happy=",Happy
+    print"Sad=",Sad
+    print"Sarcastic=",Sarcastic
+    print"Surprised=",Surprised
+    print"Crook=",Crook
+    print"Neutral=",Neutral
+    print"Angry=",Angry
+
+with open('content.txt','r') as f:
+    Happy = 0
+    Sad = 0
+    Sarcastic = 0
+    Surprised = 0
+    Crook = 0
+    Neutral = 0
+    Angry = 0
+    for line in f.readlines():
+        if line[0] == "E":
+                   for word in line.split():
+                    if word == ":)" or word == ":D":
+                            Happy = Happy + 1
+                    elif word == ":(" or word == ":'(" :
+                            Sad = Sad + 1
+                    elif word == ":P" or word == ";)" :
+                            Sarcastic = Sarcastic + 1
+                    elif word == ":-o" or word == "O_O" :
+                            Surprised = Surprised + 1
+                    elif word == "B-)" or word == ";)" :
+                            Crook = Crook + 1
+                    elif word == ":-/" or word == "=_=" :
+                            Neutral = Neutral + 1
+                    elif word == "x-(" or word == ">_<" :
+                            Angry = Angry + 1
+    print"Emoticons for E is"
+    print"Happy=",Happy
+    print"Sad=",Sad
+    print"Sarcastic=",Sarcastic
+    print"Surprised=",Surprised
+    print"Crook=",Crook
+    print"Neutral=",Neutral
+    print"Angry=",Angry
+
+with open('content.txt','r') as f:
+    Happy = 0
+    Sad = 0
+    Sarcastic = 0
+    Surprised = 0
+    Crook = 0
+    Neutral = 0
+    Angry = 0
+    for line in f.readlines():
+        if line[0] == "G":
+                   for word in line.split():
+                    if word == ":)" or word == ":D":
+                            Happy = Happy + 1
+                    elif word == ":(" or word == ":'(" :
+                            Sad = Sad + 1
+                    elif word == ":P" or word == ";)" :
+                            Sarcastic = Sarcastic + 1
+                    elif word == ":-o" or word == "O_O" :
+                            Surprised = Surprised + 1
+                    elif word == "B-)" or word == ";)" :
+                            Crook = Crook + 1
+                    elif word == ":-/" or word == "=_=" :
+                            Neutral = Neutral + 1
+                    elif word == "x-(" or word == ">_<" :
+                            Angry = Angry + 1
+    print"Emoticons for G is"
+    print"Happy=",Happy
+    print"Sad=",Sad
+    print"Sarcastic=",Sarcastic
+    print"Surprised=",Surprised
+    print"Crook=",Crook
+    print"Neutral=",Neutral
+    print"Angry=",Angry
+
+
+with open('content.txt','r') as f:
+	Happy = 0
+	Sad = 0
+	Sarcastic = 0
+	Surprised = 0
+	Crook = 0
+	Neutral = 0
+	Angry = 0
+    	for line in f:
+            for word in line.split():		 		
+		if word == ":)" or word == ":D":
+			Happy = Happy + 1
+		elif word == ":(" or word == ":'(" :
+			Sad = Sad + 1
+		elif word == ":P" or word == ";)" :
+			Sarcastic = Sarcastic + 1
+		elif word == ":-o" or word == "O_O" :
+			Surprised = Surprised + 1
+		elif word == "B-)" or word == ";)" :
+			Crook = Crook + 1
+		elif word == ":-/" or word == "=_=" :
+			Neutral = Neutral + 1
+		elif word == "x-(" or word == ">_<" :
+			Angry = Angry + 1
+Total = Happy + Sad + Sarcastic + Surprised + Crook + Neutral + Angry
+Happy = (Happy)*(100.0/Total)
+Sad = (Sad)*(100.0/Total)
+Sarcastic = (Sarcastic)*(100.0/Total)
+Surprised = (Surprised)*(100.0/Total)
+Crook = (Crook)*(100.0/Total)
+Neutral = (Neutral)*(100.0/Total)
+Angry = (Angry)*(100.0/Total)
+print"Happy :",Happy
+print"Sad :",Sad
+print"Sarcastic :",Sarcastic
+print"Surprised :",Surprised
+print"Crook :",Crook
+print"Neutral :",Neutral
+print"Angry :",Angry
+
+
+
